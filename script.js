45d70f1c396acd30f07bfff5bbe3b50ccd42dca2
indubrolk <110773586+indubrolk@users.noreply.github.com>
2025-03-19 17:46:26 +0530

    script.js edited

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const messageInput = document.querySelector(".message-input");
+const chatBody = document.querySelector(".message-input");
 
 const createMessageElement = (content, classes) => {
-    const div = document.cre
+    const div = document.createElement("div");
+    div.classList.add("message", classes);
+    div.innerHTML = content;
+    return div;
 }
 
 const handleOutgoingMessage = (userMessage) => {
     const messageContent = '<div class="message-text">${userMessage}</div>';
-    createMessageElement(messageContent, "user-message");
+    const outgoingMessageDiv = createMessageElement(messageContent, "user-message");
+    chatBody.appendChild(outgoingMessageDiv);
 }
 
 //Handle Enter key press for sending messages
